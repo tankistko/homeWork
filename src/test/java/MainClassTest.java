111ad8f49399362ea1771df9fada2c00a1c5592a
tankistko <55260335+tankistko@users.noreply.github.com>
2020-10-04 02:38:34 -0700

    1

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class MainClassTest {
@@ -23,74 +22,74 @@ public class MainClassTest {
 
     @Test
     public void scenario1() {
+        System.out.println("Scenario 1 - Verify that final results of application is correct");
         System.out.println("Step 1: Click on 'New credit line' button");
         mainPage.clickNewLineOfCredit();
         String heading = mainPage.getHeadText();
         Assert.assertEquals("New Line Of Credit", heading);
-        mainPage.typeInterestRate("35");
         System.out.println("Step 2: Type 35% on APR input field");
-        mainPage.typeCreditLimit("1000");
+        mainPage.typeInterestRate("35");
         System.out.println("Step 3: Type 1000 to 'Credit line' field");
+        mainPage.typeCreditLimit("1000");
+        System.out.println("Step 4: Click on submit button");
         mainPage.clickOnSubmitButton();
         Assert.assertEquals("Line of credit was successfully created.", mainPage.getHeadLabel());
-        System.out.println("Step 4: Click on submit button");
-        mainPage.selectDrawSelector();
         System.out.println("Step 5: Select Draw options");
-        mainPage.typeAmountInField("500");
+        mainPage.selectDrawSelector();
         System.out.println("Step 6: Type 500 in amount field");
-        mainPage.selectDay1();
+        mainPage.typeAmountInField("500");
         System.out.println("Step 7: Select first day from the list");
-        mainPage.clickSubmitButton();
+        mainPage.selectDay1();
         System.out.println("Step 8: Click 'submit' button");
+        mainPage.clickSubmitButton();
         mainPage.waitUntilPaymentResult1Appear();
-        Assert.assertEquals("Actual interest result are not matching","Interest at 30 days: $14.38",
-                mainPage.getInterestResult());
-        Assert.assertEquals("Actual total payoff result are not matching","Total Payoff at 30 days: $514.38",
-                mainPage.getTotalPayoffResult());
+        Assert.assertEquals("Actual interest result are not matching","Interest at 30 days: $14.38", mainPage.getInterestResult());
+        Assert.assertEquals("Actual total payoff result are not matching","Total Payoff at 30 days: $514.38", mainPage.getTotalPayoffResult());
     }
 
     @Test
     public void scenario2() {
+        System.out.println("Scenario 2 - Verify that final results of application is correct");
         System.out.println("Step 1: Click on 'New credit line' button");
         mainPage.clickNewLineOfCredit();
         String heading = mainPage.getHeadText();
         Assert.assertEquals("New Line Of Credit", heading);
-        mainPage.typeInterestRate("35");
         System.out.println("Step 2: Type 35% on APR input field");
-        mainPage.typeCreditLimit("1000");
+        mainPage.typeInterestRate("35");
         System.out.println("Step 3: Type 1000 to 'Credit line' field");
+        mainPage.typeCreditLimit("1000");
+        System.out.println("Step 4: Click on submit button");
         mainPage.clickOnSubmitButton();
         Assert.assertEquals("Line of credit was successfully created.", mainPage.getHeadLabel());
-        System.out.println("Step 4: Click on submit button");
-        mainPage.selectDrawSelector();
         System.out.println("Step 5: Select Draw options");
-        mainPage.typeAmountInField("500");
+        mainPage.selectDrawSelector();
         System.out.println("Step 6: Type 500 in amount field");
-        mainPage.selectDay1();
+        mainPage.typeAmountInField("500");
         System.out.println("Step 7: Select first day from the list");
-        mainPage.clickSubmitButton();
+        mainPage.selectDay1();
         System.out.println("Step 8: Click 'submit' button");
+        mainPage.clickSubmitButton();
         mainPage.waitUntilPaymentResult1Appear();
         Assert.assertEquals("Actual interest result are not matching","Interest at 30 days: $14.38", mainPage.getInterestResult());
         Assert.assertEquals("Actual total payoff result are not matching","Total Payoff at 30 days: $514.38", mainPage.getTotalPayoffResult());
         mainPage.waitUntilPaymentResult1Appear();
-        mainPage.selectPaymentSelector();
         System.out.println("Step 9: Select 'Payment' from options");
-        mainPage.typeAmountInField("200");
+        mainPage.selectPaymentSelector();
         System.out.println("Step 10: Type 200 in amount field");
-        mainPage.selectDay15();
+        mainPage.typeAmountInField("200");
         System.out.println("Step 11: Select 15th day from the list");
-        mainPage.clickSubmitButton();
+        mainPage.selectDay15();
         System.out.println("Step 12: Click 'Submit' button");
-        mainPage.waitUntilPaymentResult2Appear();
-        mainPage.selectDrawSelector();
-        System.out.println("Step 13: Select Draw options");
-        mainPage.typeAmountInField("100");
-        System.out.println("Step 14: Type 100 in amount field");
-        mainPage.selectDay25();
-        System.out.println("Step 15: Select 25th day from the list");
         mainPage.clickSubmitButton();
+        mainPage.waitUntilPaymentResult2Appear();
+        System.out.println("Step 13: Select Draw options");
+        mainPage.selectDrawSelector();
+        System.out.println("Step 14: Type 100 in amount field");
+        mainPage.typeAmountInField("100");
+        System.out.println("Step 15: Select 25th day from the list");
+        mainPage.selectDay25();
         System.out.println("Step 16: Click 'Submit' button");
+        mainPage.clickSubmitButton();
         mainPage.waitUntilPaymentResult3Appear();
         Assert.assertEquals("Actual interest result are not matching","Interest at 30 days: $11.99", mainPage.getInterestResult());
         Assert.assertEquals("Actual total payoff result are not matching","Total Payoff at 30 days: $411.99", mainPage.getTotalPayoffResult());
@@ -98,16 +97,17 @@ public class MainClassTest {
 
     @Test
     public void negativeScenario() {
-        mainPage.clickNewLineOfCredit();
+        System.out.println("Scenario 3 - Verify that error message appears when user type text in APR field");
         System.out.println("Step 1: Click on 'New credit line' button");
+        mainPage.clickNewLineOfCredit();
         String heading = mainPage.getHeadText();
         Assert.assertEquals("New Line Of Credit", heading);
-        mainPage.typeInterestRate("text");
         System.out.println("Step 2: Type text in APR field");
-        mainPage.typeCreditLimit("1000");
+        mainPage.typeInterestRate("text");
         System.out.println("Step 3: Type 1000 in amount field");
-        mainPage.clickOnSubmitButton();
+        mainPage.typeCreditLimit("1000");
         System.out.println("Step 4: Click 'Submit' button");
+        mainPage.clickOnSubmitButton();
         Assert.assertTrue("Expected error message",mainPage.errorMessageAppears());
     }
 
